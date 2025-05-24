213950ab3febc6158e3ae8731018bee5d2ca84af
SamanBozorgvarTA <saman.bozorgvar@tech-academy.io>
2025-05-24 20:41:29 +0200

    Add project files.

---
+++ AirKeyboardGUI/AirKeyboardGUI.cpp
@@ -0,0 +1,12 @@
+﻿// AirKeyboardGUI.cpp : Defines the entry point for the application.
+//
+
+#include "AirKeyboardGUI.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
